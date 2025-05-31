7606264763056d67b5a904fd451d2173ba82339f
ekkymulia <ekky.mulialasardi@gmail.com>
2025-05-31 20:41:21 +0900

    change to eatsseason

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,13 +3,14 @@ import { EnvVarWarning } from "@/components/env-var-warning";
 import HeaderAuth from "@/components/header-auth";
 import { ThemeSwitcher } from "@/components/theme-switcher";
 import { hasEnvVars } from "@/utils/supabase/check-env-vars";
-import { Geist } from "next/font/google";
+import { Geist, Gowun_Batang, Inter } from "next/font/google";
 import { ThemeProvider } from "next-themes";
 import Link from "next/link";
 import "./globals.css";
 
 import { NextIntlClientProvider } from "next-intl";
 import FooterNav from "@/components/footer";
+import Image from "next/image";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -26,13 +27,27 @@ const geistSans = Geist({
   subsets: ["latin"],
 });
 
+const gowunBatang = Gowun_Batang({
+  weight: ['400', '700'], // Specify the weights you need
+  subsets: ['latin'], // Or other subsets if required
+  variable: '--font-gowun-batang', // Optional: CSS variable name
+  display: 'swap', // Recommended for performance
+});
+
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter', // Optional: CSS variable name
+  display: 'swap',
+});
+
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={geistSans.className} suppressHydrationWarning>
+    <html lang="en" className={`${gowunBatang.variable} ${inter.variable}`} suppressHydrationWarning>
       <body className="bg-background text-foreground">
         <NextIntlClientProvider>
           <ThemeProvider
@@ -41,22 +56,28 @@ export default function RootLayout({
             enableSystem
             disableTransitionOnChange
           >
-            <main className="h-screen flex flex-col items-center px-4 sm:px-6 md:px-8">
+            <main className="h-screen flex flex-col items-center">
               {/* Sticky Header */}
-              <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16 sticky top-0 z-10 bg-background">
+              <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16 sticky top-0 z-10 bg-[#23532A]">
                 <div className="flex flex-col sm:flex-row w-full max-w-5xl justify-between items-center py-3 px-4 sm:px-5 text-sm gap-2 sm:gap-0">
                   <div className="flex gap-4 items-center font-semibold text-base sm:text-sm flex-wrap">
-                    <Link href={"/"}>FestiveMeet</Link>
+                    <Link href={"/"}>
+                    <Image
+                      src="/images/eatseason_white_horizontal_logo.png" // Path should be relative to the public directory
+                      alt="EatSeason Logo" // Always add descriptive alt text for accessibility
+                      width={100} // Specify the width of the image
+                      height={50} // Specify the height of the image
+                      // You might also consider adding 'priority' if this is an important LCP image
+                      // priority
+                    />
+                    </Link>
                   </div>
-                  {!hasEnvVars ? (
-                    <EnvVarWarning />
-                  ) : (
+             
                     <HeaderAuth />
-                  )}
                 </div>
               </nav>
 
-              <div className="flex flex-col gap-5 w-full max-w-5xl p-4 sm:p-5 flex-1 overflow-y-auto">
+              <div className="w-full flex-1 overflow-none">
                 {children}
               </div>
 
